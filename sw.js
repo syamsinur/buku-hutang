68f75459659ba8947129b890c5d2e145556a0b43
Nur Syamsi <syamsinur455@gmail.com>
2026-02-13 14:02:04 +0700

    fix sw 5

--- sw.js
+++ sw.js
@@ -1,3 +1,12 @@
+const CACHE_NAME = 'buku-hutang-v1';
+
+self.addEventListener('install', (event) => {
+  console.log('Service Worker: Installed');
+});
+
 self.addEventListener('fetch', (event) => {
-    event.respondWith(fetch(event.request));
+  // Handler ini wajib ada agar syarat PWA terpenuhi
+  event.respondWith(
+    fetch(event.request).catch(() => caches.match(event.request))
+  );
 });
