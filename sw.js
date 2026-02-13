939355c43b2a227154ffe1a5b467653e5fad9b24
Nur Syamsi <syamsinur455@gmail.com>
2026-02-13 13:55:42 +0700

    fix sw 4

--- sw.js
+++ sw.js
@@ -1,8 +1,3 @@
-// sw.js (di folder root)
-self.addEventListener('install', (event) => {
-  console.log('Service Worker: Installed');
-});
-
 self.addEventListener('fetch', (event) => {
-  // pwa
+    event.respondWith(fetch(event.request));
 });
